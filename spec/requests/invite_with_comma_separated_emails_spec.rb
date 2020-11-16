f3c367f9da15d8daa42c04a21e1931620905e5ad
Marc Assens <marc.a.r95@gmail.com>
2020-11-16 14:43:18 +0100

    test: add test

--- spec/requests/invite_with_comma_separated_emails_spec.rb
+++ spec/requests/invite_with_comma_separated_emails_spec.rb
@@ -50,4 +50,46 @@ describe 'api' do
       expect(invites[1]['role']).to eq 'guest'
     end
   end
+
+  context 'invite with duplicated emails' do
+    subject do
+      do_post invites_path,
+           params: { invite: { invitable_id: @company.id,
+                               invitable_type: @company.class.name,
+                               email: ["gaug@gmail.com", "gaug2@gmail.com", "gaug@gmail.com"].join(', '),
+                               role: 'guest' } },
+           **json_headers()
+    end
+
+    it 'returns json' do
+      sign_in_with @user
+      subject
+      expect(response.content_type).to eq('application/json')
+    end
+
+    it 'responds with success' do
+      sign_in_with @user
+      subject
+      expect(response).to have_http_status(201) # 201 is http status 'created'
+    end
+
+    it 'responds with json invites' do
+      sign_in_with @user
+      subject
+      invites = JSON.parse(response.body)
+      expect(invites.count).to be 2
+      expect(invites[0]['email']).to eq email1
+      expect(invites[1]['email']).to eq email2
+      expect(invites[0]['recipient_id']).to be nil
+      expect(invites[1]['recipient_id']).to be nil
+      expect(invites[0]['sender_id']).to eq @user.id
+      expect(invites[1]['sender_id']).to eq @user.id
+      expect(invites[0]['invitable_id']).to eq @company.id
+      expect(invites[1]['invitable_id']).to eq @company.id
+      expect(invites[0]['invitable_type']).to eq @company.class.name
+      expect(invites[1]['invitable_type']).to eq @company.class.name
+      expect(invites[0]['role']).to eq 'guest'
+      expect(invites[1]['role']).to eq 'guest'
+    end
+  end
 end
